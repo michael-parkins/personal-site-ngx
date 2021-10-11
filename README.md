de028a3cd0318b80c7dd3ce1d398888e873839fd
Michael <michael.e.parkins@hotmail.com>
2021-10-11 18:15:07 -0400

    Add base routes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [![CI Build](https://github.com/michael-parkins/personal-site-ngx/actions/workflows/build.yml/badge.svg)](https://github.com/michael-parkins/personal-site-ngx/actions/workflows/build.yml)
+[![CI Deploy](https://github.com/michael-parkins/personal-site-ngx/actions/workflows/deploy.yml/badge.svg)](https://github.com/michael-parkins/personal-site-ngx/actions/workflows/deploy.yml)
 
 # PersonalSiteNgx
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
