c908dcf8a517cc8905ace2126f24bf890c64ae80
Michael <michael.e.parkins@hotmail.com>
2022-01-08 00:09:52 -0500

    maintain:clean up tailwind build warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   mode: 'jit',
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.{html,ts}'],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'media',
   theme: {
     extend: {},
   },
