f13c3b830fb8dc38939c6c88b335bb9c9475f106
Michael <michael.e.parkins@hotmail.com>
2022-02-04 01:23:21 -0500

    feature: add icons, expand greeting area

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,18 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'open-sans': ['"Open Sans"', 'sans-serif'],
+      },
+      height: {
+        'screen-1/2': '50vh',
+        'screen-3/4': '75vh',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
