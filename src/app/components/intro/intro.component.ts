3060108c88fb66fdb877b4a0c71b16d9e3f30c85
Michael <michael.e.parkins@hotmail.com>
2022-02-28 19:43:51 -0500

    feature: new intro

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,9 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { AnimationService } from 'src/app/modules/shared/services/animation.service';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
 })
-export class IntroComponent {
-  constructor() {}
+export class IntroComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('personalList') personalListRef!: ElementRef;
+  personalList = ['husband', 'father', 'LEGO builder', 'MCU super fan'];
+  intervalId: ReturnType<typeof setInterval> | undefined;
+  activeIndex = 0;
+
+  constructor(private animationService: AnimationService) {}
+
+  ngAfterViewInit(): void {
+    this.iterateText();
+    this.animate();
+    setTimeout(() => {
+      this.animate();
+    }, 3500);
+
+    this.intervalId = setInterval(() => {
+      this.animate();
+      setTimeout(() => {
+        this.animate();
+      }, 2500);
+      setTimeout(() => {
+        this.iterateText();
+      }, 3500);
+    }, 4000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  animate(): void {
+    this.animationService.toggleAnimationClasses(this.personalListRef, 'w-0', 'w-3/4');
+  }
+
+  iterateText() {
+    this.personalListRef.nativeElement.textContent = this.personalList[this.activeIndex];
+    this.activeIndex < this.personalList.length - 1 ? this.activeIndex++ : (this.activeIndex = 0);
+  }
 }
