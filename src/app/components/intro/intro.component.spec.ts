21b2d4e219edcb71cb7c003796dc064cdc058c4f
Michael <michael.e.parkins@hotmail.com>
2022-02-28 20:21:42 -0500

    bug: undo ignored test

--- src/app/components/intro/intro.component.spec.ts
+++ src/app/components/intro/intro.component.spec.ts
@@ -1,14 +1,23 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { Spectator, createComponentFactory, SpyObject, createSpyObject } from '@ngneat/spectator';
+import { AnimationService } from 'src/app/modules/shared/services/animation.service';
 import { IntroComponent } from './intro.component';
 
-xdescribe('IntroComponent', () => {
+describe('IntroComponent', () => {
   let spectator: Spectator<IntroComponent>;
   const createComponent = createComponentFactory({
     component: IntroComponent,
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
   });
-  beforeEach(() => (spectator = createComponent()));
+  let animationService: SpyObject<AnimationService>;
+  animationService = createSpyObject(AnimationService);
+
+  beforeEach(
+    () =>
+      (spectator = createComponent({
+        providers: [{ provide: AnimationService, useValue: animationService }],
+      }))
+  );
 
   it('should render', () => {
     expect(spectator.query('h1')?.textContent).toEqual(`Hi, I'm Michael,`);
