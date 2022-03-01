56873ff6339c4bfa0326914546edbf0e8509491e
Michael <michael.e.parkins@hotmail.com>
2022-03-01 00:28:31 -0500

    feature: animation starts without delay

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -7,28 +7,26 @@ import { AnimationService } from 'src/app/modules/shared/services/animation.serv
 })
 export class IntroComponent implements AfterViewInit, OnDestroy {
   @ViewChild('personalList') personalListRef!: ElementRef;
-  personalList = ['husband', 'father', 'LEGO builder', 'MCU super fan'];
+  personalList = [
+    'husband',
+    'father',
+    'dog lover',
+    'gamer',
+    'LEGO builder',
+    'Star Wars fan',
+    'ramen foodie',
+    'MCU super fan',
+    'bushcraft geek',
+  ];
   intervalId: ReturnType<typeof setInterval> | undefined;
   activeIndex = 0;
 
   constructor(private animationService: AnimationService) {}
 
   ngAfterViewInit(): void {
-    this.iterateText();
-    this.animate();
-    setTimeout(() => {
-      this.animate();
-    }, 3500);
-
-    this.intervalId = setInterval(() => {
-      this.animate();
-      setTimeout(() => {
-        this.animate();
-      }, 2500);
-      setTimeout(() => {
-        this.iterateText();
-      }, 3500);
-    }, 4000);
+    this.createInterval();
+    this.initText();
+    this.executeAnimationSteps();
   }
 
   ngOnDestroy(): void {
@@ -37,12 +35,32 @@ export class IntroComponent implements AfterViewInit, OnDestroy {
     }
   }
 
+  initText() {
+    this.iterateText();
+  }
+
+  createInterval() {
+    this.intervalId = setInterval(() => {
+      this.executeAnimationSteps();
+    }, 4000);
+  }
+
+  executeAnimationSteps() {
+    this.animate();
+    setTimeout(() => {
+      this.animate();
+    }, 2500);
+    setTimeout(() => {
+      this.iterateText();
+    }, 3500);
+  }
+
   animate(): void {
     this.animationService.toggleAnimationClasses(this.personalListRef, 'w-0', 'w-3/4');
   }
 
   iterateText() {
-    this.personalListRef.nativeElement.textContent = this.personalList[this.activeIndex];
+    this.personalListRef.nativeElement.textContent = this.personalList[this.activeIndex] + '.';
     this.activeIndex < this.personalList.length - 1 ? this.activeIndex++ : (this.activeIndex = 0);
   }
 }
