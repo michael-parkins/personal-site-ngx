5e4a895dfea48dbccf4c2ed39ebc48ccc5ab925a
Michael <michael.e.parkins@hotmail.com>
2022-03-12 08:38:47 -0500

    feature: add dev-to social

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { Social } from 'src/models/social';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
+  socials: Social[] = [
+    {
+      name: 'Github',
+      icon: 'github',
+      url: 'https://github.com/michael-parkins',
+    },
+    {
+      name: 'Twitter',
+      icon: 'twitter',
+      url: 'https://twitter.com/d00dparkins',
+    },
+    {
+      name: 'Dev.to',
+      icon: 'dev-to',
+      url: 'https://dev.to/michaelparkins',
+    },
+    {
+      name: 'LinkedIn',
+      icon: 'linkedin',
+      url: 'https://www.linkedin.com/in/michael-parkins/',
+    },
+  ];
+
   constructor() {}
 }
---
+++ src/models/social.ts
@@ -0,0 +1,5 @@
+export interface Social {
+  name: string;
+  icon: string;
+  url: string;
+}
