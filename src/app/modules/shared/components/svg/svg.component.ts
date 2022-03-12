41eec5536578e54a86224efa46a28a500d4719ff
Michael <michael.e.parkins@hotmail.com>
2022-03-11 19:49:03 -0500

    refactor: clean all svg refs

--- src/app/modules/shared/components/svg/svg.component.ts
+++ src/app/modules/shared/components/svg/svg.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SvgComponent implements OnInit {
   @Input() name = '';
   @Input() title = '';
+  @Input() classList = '';
   url = '';
 
   constructor() {}
